5a9a3828ad960bd7e7ff3f40c0ffdf85beea4f9a
Ubuntu <roelh@ubuntuguiroel.c3u4n5qzsg4unkfdtsbhgdbehg.zx.internal.cloudapp.net>
2024-04-01 11:26:08 +0000

    Initial commit

---
+++ website/src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="app-holder">
+  <HelloWorld :title="'Vrije Universiteit Amsterdam'" :content="'Time At the vu.'" :image="'VU.jpg'" />
+ </div>
+</template>
+
+<style scoped>
+
+#app {
+    margin: 0 0;
+    padding:0 0;
+}
+.app-holder{
+  left:0;
+  top:0;
+  position:absolute;
+  width:100vw;
+  height:100vh;
+}
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ website/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+
+defineProps(['title','content','image']);
+
+</script>
+
+<template>
+  <div class="section-holder">
+    <div class="picture-holder">
+      <div class="card-format">
+        <h1>{{title}}</h1>
+        <p>{{content}}</p>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+.card-format{
+  left:0;
+  width:40%;
+  height:65%;
+  opacity:0.5;
+}
+.section-holder{
+  display:flex;
+  flex-direction:row;
+  width:100vw;
+}
+.picture-holder{
+  padding: 6rem 4rem;
+  display:flex;
+  flex-direction:row;
+  background-size:cover;
+  background-image: url('"../assets/imgs/" + "VU.jpg"');
+  width:100vw;
+  height:75vh;
+  min-height: 500px;
+}
+
+
+</style>
