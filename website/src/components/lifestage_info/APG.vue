0097527b91b377e6eace90c3c2bab7c1d9354829
Roel Huijskens <45594735+RoelHuijskens@users.noreply.github.com>
2024-07-17 12:42:39 +0200

    Small website text update

--- website/src/components/lifestage_info/APG.vue
+++ website/src/components/lifestage_info/APG.vue
@@ -4,11 +4,11 @@
         <h1 class="title-card-holder-full">APG Asset Management</h1>
         <h3>2021-now</h3>
         <p>
-            After my studies I started working as a Devops engineer at APG Asset Management.
-            In this role, I support our business with data analytics tooling such as Posit, R, Python.
-            This involves both consulting our business on how to develop and deploy Python/R/Alteryx applications and help maintaining our infrastructure.
-            During this time, I have strongly developed my technical skills and have learned a lot about the inner workings of a large financial institution.
-            I get the most satisfaction from my work when I can help our portfolio managers with their data problems and helping them develop better technical skills.
+            After completing my studies, I began my career as a DevOps Engineer at APG Asset Management.
+            In this role, I support our business with data analytics tools, including Posit, R, and Python.
+            My responsibilities involve consulting with our business units on developing and deploying Python, R, and Alteryx applications, as well as maintaining our infrastructure.
+            This experience has significantly enhanced my technical skills and provided me with valuable insights into the operations of a large financial institution.
+            I find the most satisfaction in my work when assisting our portfolio managers with their data challenges and helping them improve their technical skills.
         </p>
     </div>
     </div>
@@ -24,7 +24,7 @@
                <li>Posit (workbench/connect)</li>
                <li>Databricks</li>
                <li>Azure Devops (Azure Pipelines/ci/cd)</li>
-               <li>Vue</li>
+               <li>VUE</li>
                <li>LLMs</li>
                <li>SQL</li>
                <li>Git</li>
@@ -42,7 +42,7 @@
     <div class="card-format card-appear" :style="{animationDuration: '2s'}">
         <h2 class="title-card-holder-full">Projects</h2>
         <p>
-            I have worked on several projects during my time at apg, some of which are:
+            I have worked on several projects during my time at APG, some of which are:
 
             <ul>
                 <li>
@@ -56,11 +56,19 @@
                 </li>
                 <li>
                     <b>Developed a Data interface for Bloomberg Market data.</b>
-                    <p>As apg was moving away from excel sheets to a more mature data platform for </p>
+                    <p>
+                        As APG was moving away from excel sheets to a more mature data platform, there was a requirment for obtaining static reference market data on an adhoc basis from the Bloomberg Data License platform.
+                        I developed an API which serves our portfolio manager market data on a per request basis. Individual requests were matched against a caching database to ensure no unnecessary costs were made when requesting data.
+                        The api was build in Fastapi and utilized a distributed worker system using celery for completing individual requests. All hosted on rhel8 servers on premise.
+                    </p>
                 </li>
                 <li>
                     <b>Developed and maintained a complex ansible stack for our platforms.</b>
-                    <p>As part of our ESG strategy we needed to integrate the MSCI ESG data into our data platform</p>
+                    <p>
+                        Our teams responsibilities grew rapidly during my years at APG asset management.  It was therefore instrumental to be in full control of all our tools and applications, and ensure predictability for the uptime of our platforms.
+                        I made signifcant improvements on our Ansible stack to manage all our linux servers (20 + ) allowing our team to deploy changes to dev/acceptance and production.
+                        I futher created several automated test suites which would notify our team if any service or functionality was offline for ensuring functionial continuity of our platform.
+                    </p>
                 </li>
                 </ul>
         </p>
