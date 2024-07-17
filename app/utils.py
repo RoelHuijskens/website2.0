2d23b664ad7e734629931abae42c5f342f9c8859
Roel Huijskens <45594735+RoelHuijskens@users.noreply.github.com>
2024-07-17 12:00:13 +0200

    Test to see if pipelines fail

--- app/utils.py
+++ app/utils.py
@@ -33,6 +33,7 @@ def generate_security_hash(seed: str) -> str:
     Returns:
         str: Result of sha256 hash
     """
+    raise ValueError("oh lawd")
 
     salt = os.getenv("ADMIN_HASH")
     if not salt:
