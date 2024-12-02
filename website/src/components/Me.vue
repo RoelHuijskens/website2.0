d91b9add527931a14ecc78c7aafea4eb9d909d62
Roel Huijskens <45594735+RoelHuijskens@users.noreply.github.com>
2024-12-02 22:54:25 +0100

    Fixed current personal pitch (#5)
    * Massive formatting + testing refactor
    
    * Add testing pipeline
    
    * update python version pipeline
    
    * Fix script call
    
    * Update python-testing.yml
    
    * add
    
    * rename testing scripts
    
    * Update python-testing.yml
    
    * Update python-testing.yml
    
    * Fix typo linting file name
    
    * Test to see if pipelines fail
    
    * publish test results
    
    * Small website text update
    
    * Update requirements.txt file createion
    
    * update git ignore
    
    * add
    
    * remove cached pyc files
    
    * Update knowledge back
    
    * fix response handling
    
    * Updates fronted
    
    * Fix handling
    
    * Update
    
    * update resume link
    
    * Update personal info

--- website/src/components/Me.vue
+++ website/src/components/Me.vue
@@ -4,8 +4,8 @@
         <h1>About Me</h1>
         <p>
             Good day to you and welcome to my site! I keep this place around for experimentation and sharing some info about myself.
-            My name is Roel Huijskens, I am {{ this.getAge() }} years old and currently live in The Hague. I am passionate about software development, AI, and data engineering, which are integral to my daily work. You can learn more about these interests below.
-            Currently, I work at APG Asset Management as a DevOps Engineeer. Before I started at APG I completed my masters degree in applied statistics at Utrecht University and my bachelors degree in psychology at the VU University.
+            My name is Roel Huijskens, I am {{ this.getAge() }} years old and currently live in The Hague. I am passionate about technology, software development, AI, and data engineering, which are integral to my daily work. You can learn more about these interests below.
+            Currently, I work at at Stedin as a Platform Engineeer. Before I started at Stedin I worked as a DevOps Engineer at APG. Before getting a 'real' job I completed my masters degree in applied statistics at Utrecht University and my bachelors degree in psychology at the VU University.
             In my free time I also like to do stuff away from my PC such as working out, cycling, watching movies.
             If you have any questions, feel free to ask them in the chat above or get linked on linkedin.
         </p>
@@ -18,7 +18,7 @@ export default {
     methods :  {
         getAge() {
             var today = new Date();
-            var birthDate = new Date("1996-12-21");
+            var birthDate = new Date("1996-11-21");
             var age = today.getFullYear() - birthDate.getFullYear();
             var m = today.getMonth() - birthDate.getMonth();
             if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
